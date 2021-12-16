09234be00335853386d7f1d9afdf84018c7a54d4
Christopher Tolo <ctolo@users.noreply.github.com>
2021-12-16 14:06:59 -0800

    commit

---
+++ iq-find-log4j/README.md
@@ -0,0 +1,11 @@
+# find-log4j
+This is a sample script to search all applications that have been scanned by Lifecycle for the log4j-core components.
+
+## Requirements
+Developed for python 3 using the component requests.
+
+## Directions
+You will need to update the script with the base url for your IQ Server.  You will also need to update the username and password in the script before running.
+
+The script will output the applications where log4j is found to the console as well as a results.json file.  Feel free to adapt the output to your needs.
+
---
+++ iq-find-log4j/iq-find-log4j.py
@@ -0,0 +1,68 @@
+import requests
+import json
+from urllib.parse import urlencode
+
+# update IQ server location, username, and password
+iq_url = "http://localhost:8070"
+username = "admin"
+password = "admin123"
+
+iq_session = requests.Session()
+iq_session.auth = requests.auth.HTTPBasicAuth("admin", "admin123")
+stages = ["build","stage-release","release","operate"]
+results = []
+
+print("--------------------------------------")
+
+## test for IQ server
+try:
+	url = f"{iq_url}/api/v2/applications"
+	response = iq_session.get(url)
+	response.raise_for_status()
+
+except (iq_session.exceptions.ConnectionError, iq_session.exceptions.Timeout):
+	print("Cannot find IQ Server, check URL")
+	exit()
+
+except iq_session.exceptions.HTTPError:
+	print("Could not authenticate, check username and password")
+	exit()
+
+url = f"{iq_url}/rest/product/version"
+response = iq_session.get(url)
+version = response.json()['version']
+print(f'IQ Server Version {version}')
+
+component = {
+	"componentIdentifier" : {
+		"format" : "maven", 
+		"coordinates" : {
+			"groupId" : "org.apache.logging.log4j",
+			"artifactId" : "log4j-core",
+			"version" : "2.*"
+		}
+	}
+}
+
+for stage in stages:
+	component['stageId'] = stage
+	params = urlencode(component).replace("%27", "%22")
+	url = f"{iq_url}/api/v2/search/component?{params}"
+	response = iq_session.get(url)
+
+	for result in response.json()['results']:
+		# to reduce noise in results
+		del result["componentIdentifier"]
+		result['stage'] = stage
+		results.append(result)
+
+output = json.dumps(results, indent=4)
+
+print(output)
+print("--------------------------------------")
+with open("results.json", "w+") as file:
+	file.write(output)
+	print("Json results saved to -> results.json")
+
+print("--------------------------------------")
+
