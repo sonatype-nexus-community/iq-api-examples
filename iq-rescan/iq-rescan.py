7563fef6e4ed956d3d6a821dc20504ff5a5e209e
Christopher Tolo <ctolo@users.noreply.github.com>
2021-12-21 10:59:50 -0800

    adding arg handling to rescan
    can now use passed in args, added filter to skip dev and operate stages.

--- iq-rescan/iq-rescan.py
+++ iq-rescan/iq-rescan.py
@@ -2,13 +2,28 @@ import requests
 import json
 import time
 import datetime
+import argparse
 
-iq_session = requests.Session()
-iq_session.auth = requests.auth.HTTPBasicAuth("admin", "admin123")
-iq_url = "http://localhost:8070"
+iq_url, iq_session, interval, skip = "", {}, 100, []
 today = datetime.datetime.now()
-interval = 7
+
+def get_arguments():
+	global iq_url, iq_session, interval, skip
+	parser = argparse.ArgumentParser(description='List applications that have not scanned for a given number of days.')
+	parser.add_argument('-u', '--url', help='', default="http://localhost:8070", required=False)
+	parser.add_argument('-a', '--auth', help='', default="admin:admin123", required=False)
+	parser.add_argument('-d', '--interval', help='', default=7, required=False)
+	parser.add_argument('-s', '--skip', help='', default="develop:operate", required=False)
+	args = vars(parser.parse_args())
+	iq_url = args["url"]
+	interval = int(args['interval'])
+	creds = args["auth"].split(":")
+	skip = args["skip"].split(":")
+	iq_session = requests.Session()
+	iq_session.auth = requests.auth.HTTPBasicAuth(creds[0], creds[1])
+	return args
 #-------------------------------------------------------------
+args = get_arguments()
 print('-'*60)
 print(f'Starting rescan for reports older than {interval} days.')
 # Find all applications
@@ -30,7 +45,7 @@ for app in apps:
 		dt = report['evaluationDate']
 		days = (today - datetime.datetime.fromisoformat(dt.replace('Z', '')).replace(tzinfo=None)).days
 		print(f"Stage : '{stage}'' was scanned {days} days ago.")
-		if days < interval: print('-- skipping'); continue
+		if days < interval or stage in skip: print('-- skipping'); continue
 		#----------------------------------
 		print('rescanning...')	
 		# Promoting last scan for stage to the same stage.
