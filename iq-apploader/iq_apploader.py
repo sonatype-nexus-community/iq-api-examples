40838479b0c26b1c7885ea6f89632cbe489ca37c
Christopher Tolo <ctolo@users.noreply.github.com>
2021-12-22 13:06:13 -0800

    Update iq_apploader.py

--- iq-apploader/iq_apploader.py
+++ iq-apploader/iq_apploader.py
@@ -5,7 +5,11 @@ import subprocess
 import json
 import requests
 
-iq_url, iq_auth, iq_session, iq_headers = "", "", "", ""
+iq_url, iq_auth, basepath, file_name, creds, iq_version, iq_cli = "", "", "", "", ['',''], "", ""
+iq_session = requests.Session()
+iq_headers = {'X-CSRF-TOKEN': 'api'}
+iq_session.cookies.set('CLM-CSRF-TOKEN', 'api')
+
 extensions = [
 	".jar",".war",".ear",".zip",
 	".tar",".tar.bz2",".tar.gz",
@@ -13,58 +17,68 @@ extensions = [
 	".dll",".js",".whl",".egg",".rpm"
 ]
 
-def main():
-	global iq_url, iq_session, iq_auth, iq_headers
+def get_arguments():
 	parser = argparse.ArgumentParser(description='Add and scan applications in the basepath folder.  Uses auto applications feature for id that are not already present.')
 	parser.add_argument('-a','--auth', default="admin:admin123", required=False)
 	parser.add_argument('-u','--url', default="http://localhost:8070", required=False)
 	parser.add_argument('-b','--basepath', default="apps/", required=False)
 	parser.add_argument('-f','--file_name', default="export.csv", required=False)
-	args = vars(parser.parse_args())
-	iq_url, basepath, file_name = args["url"], args["basepath"], args["file_name"]
-	iq_session = requests.Session()
-	iq_auth = args["auth"] 
-    creds = iq_auth.split(":")
-	iq_headers = {'X-CSRF-TOKEN': 'api'}
-	iq_session.cookies.set('CLM-CSRF-TOKEN', 'api')
-    iq_session.auth = requests.auth.HTTPBasicAuth(creds[0], creds[1]) 
-	#----------------------------------------------------------------------
-	
-	# Turn on auto-application creation if needed
-	auto_applications()
+	return vars(parser.parse_args())
 
-	apps = get_apps(args["basepath"])
-	export_csv(apps, file_name)
+def main():
+	global iq_url, basepath, file_name, creds, iq_version, iq_cli, iq_auth
+	args = get_arguments()
+	iq_url = args["url"] 
+	basepath = args["basepath"]
+	file_name = args["file_name"]
+	iq_auth = args["auth"]
+	creds = iq_auth.split(":")
+	iq_session.auth = requests.auth.HTTPBasicAuth(creds[0], creds[1]) 
+	#----------------------------------------------------------------------
+	iq_version = get_iq_version()
+	iq_cli = f'nexus-iq-cli-{iq_version}.jar'
+	get_cli()
+
+	# Test auto-application creation
+	test_auto_applications()
+	
+	if os.path.isfile(file_name):
+		print('Found csv file')
+	else:
+		apps = get_apps()
+		export_csv(apps)
 
 	# Step 3. Working through scan list; update temp file of finished scans/exitcodes/links.
-	scanAppsCSV(file_name)
+	scanAppsCSV()
 
 	#----------------------------------------------------------------------
 #--------------------------------------------------------------------------
 
-def auto_applications():
-	resp = iq_session.get(f'{iq_url}/rest/config/automaticApplications').json()
-	po = resp["parentOrganizationId"]
+def test_auto_applications():
+	url = f"{iq_url}/rest/config/automaticApplications"
+	resp = iq_session.get(url).json()
 	if not resp["enabled"]:
-		data = {'enabled': True, 'parentOrganizationId': po}
-		iq_session.put( url , json=data, headers=iq_headers)
+		print('Auto Applications is not configured.  Please turn it on before continuing.')
+		exit()
 
-def get_apps(basepath):
+def get_apps():
 	apps= []
+	print(f'Looking for applications in "{basepath}"')
 	for root, dirs, files in os.walk(basepath):
 		for name in files:
+			print(f"-- found: {name}")
 			filenames = os.path.splitext(name)
 			publicId = filenames[0].replace(" ", "_")
 			extension = filenames[1]
 			paths = os.path.join(root, name)
 			app = {"publicId": publicId, "extension": extension, 
 					"path": paths, "status": "", "link": ""}
-
 			if extension in extensions:
 				apps.append(app)
+	print(f"Found {len(apps)} apps")
 	return apps
 
-def export_csv(apps, file_name):
+def export_csv(apps):
 	if len(apps) > 0:	
 		head = list(apps[0].keys())
 		with open(file_name, mode='w') as file:
@@ -73,18 +87,17 @@ def export_csv(apps, file_name):
 			for app in apps:
 				writer.writerow(app)
 
-def scanAppsCSV(file_name):
-	cli = get_cli()
-
+def scanAppsCSV():
 	with open(file_name, mode='r+') as file, open("temp_"+file_name, 'w') as temp:
 		c, apps = 0, csv.DictReader(file)
 		results = csv.DictWriter(temp, fieldnames=apps.fieldnames)
 		results.writeheader()
 		for app in apps:
-			publicId, path = app['publicId'], app["path"]
+			publicId, path, report_url = app['publicId'], app["path"], ""
+			print("-"*80)
 			print(f"Starting scan: {publicId}")
-
-			cmd = ["java", "-jar", cli, "-s", iq_url, "-a", iq_auth, "-i", publicId, path]
+			print("-"*80)
+			cmd = ["java", "-jar", iq_cli, "-s", iq_url, "-a", iq_auth, "-i", publicId, path]
 			process = subprocess.Popen(cmd, bufsize=1, universal_newlines=True, 
 					stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 			for line in iter(process.stdout.readline, ''):
@@ -99,25 +112,21 @@ def scanAppsCSV(file_name):
 			#if c ==1: break
 		print(f'Processed {c} applications.')
 
-def get_cli():
-	iq_version = get_iq_version()
-	print(f'IQ Server Version {iq_version}')
-	cli = f'nexus-iq-cli-{iq_version}.jar'
-	download_cli(cli)
-	return cli
-
-def download_cli(cli):
-	if not os.path.isfile(cli):
-		print(f'Downloading CLI: {cli}')
-		url = f'https://download.sonatype.com/clm/scanner/{cli}'
-		resp = requests.Session().get(url)
-		with open(cli, 'wb') as file:
-			file.write(resp.content)	
-
 def get_iq_version():
 	url = f'{iq_url}/rest/user-telemetry/config'
 	resp = iq_session.get(url).json()
 	return resp["account"]["iq-server-version"]
+
+def get_cli():
+	if not os.path.isfile(iq_cli):
+		print(f'Downloading CLI: {iq_cli}')
+		url = f'https://download.sonatype.com/clm/scanner/{iq_cli}'
+		resp = requests.Session().get(url)
+		with open(iq_cli, 'wb') as file:
+			file.write(resp.content)	
+
+def pp(page):
+    print(json.dumps(page, indent=4))
 
 #--------------------------------------------------------------------
 if __name__ == "__main__":
